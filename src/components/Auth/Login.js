5b1df35484755c13f6893dbdd7d1951cea88d44a
Pushpender Singh <36353150+pushpendersingh97@users.noreply.github.com>
2024-03-28 21:07:35 +0530

    Netflix Logo and header logo added

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Header } from "../common/Header";
+import { BACKGROUND_IMG } from "../../utils/constants";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className=" w-full bg-gradient-to-b from-black text-white">
+      <div className="absolute w-full -z-10">
+        <img
+          src={BACKGROUND_IMG}
+          alt="Background"
+          className="h-[100vh] w-full"
+        />
+      </div>
+      <div className="container mx-auto z-10 h-[100vh]">
+        <Header />
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
---
+++ src/components/common/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="conatiner flex justify-between items-center">
+      <div className="h-20 my-4">
+        <img src="/Netflix_Logo_PMS.png" alt="Logo" className="h-20" />
+      </div>
+    </div>
+  );
+};
